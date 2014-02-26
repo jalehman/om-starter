b555e404ff34072b271a92a7f9d0c6d04480cf15
Petrus <github@freshcode.co.za>
2014-02-26 15:49:38 +0200

    updated project.clj to latest version of [om "0.5.0"]

--- src/leiningen/new/om_starter/project.clj
+++ src/leiningen/new/om_starter/project.clj
@@ -12,7 +12,7 @@
                  [org.clojure/clojurescript "0.0-2138"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [cljs-http "0.1.3"]
-                 [om "0.1.7"]]
+                 [om "0.5.0"]]
 
   :plugins [[lein-cljsbuild "1.0.1"]
             [lein-ring "0.8.7"]
